1371884ba278035e2ee7ff88333cc75ea9f53865
Carl1101 <carlosvazquez014@gmail.com>
2014-10-17 16:54:17 -0400

    Added angular.js to to main.js

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,12 +8,44 @@
  * Controller of the hubAppApp
  */
 angular.module('hubAppApp').controller('MainCtrl', function($scope) {
-    $scope.awesomeThings = [
-        'HTML5 Boilerplate',
-        'AngularJS',
-        'Karma'
-    ];
+
+    $scope.basicForm = {
+    	"username": '',
+    	"firstName": '',
+    	"lastName": '',
+    	"email": '',
+    	"password": '',
+    	"retypedPassword": ''
+    };
+
+    $scope.studentForm = {
+    	"city": '',
+    	"state": '',
+    	"zipCode": '',
+    	"facebook": '',
+    	"twitter": '',
+    	"googleId": '',
+    	"gravatar": '',
+    	"institution": '',
+    	"department": ''
+    };
+
+    $scope.userForm = {
+    	"title": '',
+    	"city": '',
+    	"state": '',
+    	"zipCode": '',
+    	"facebook": '',
+    	"twitter": '',
+    	"googleId": '',
+    	"gravatar": ''
+    };
     
+    $scope.submit = function() {
+    	console.log($scope.basicForm);
+    	console.log($scope.studentForm);
+    	console.log($scope.userForm);
+    }
     $scope.$on('$viewContentLoaded', function() {
         document.getElementById("primaryNav").style.backgroundColor = "rgba(0, 0, 0, 0.74)";
     });
