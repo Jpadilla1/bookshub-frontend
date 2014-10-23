6702ab4011dfdf4787818a1b775699e05e66a282
Carl1101 <carlosvazquez014@gmail.com>
2014-10-23 15:00:17 -0400

    Changed Sign Up Page

--- app/scripts/controllers/signUp.js
+++ app/scripts/controllers/signUp.js
@@ -2,50 +2,67 @@
 
 /**
  * @ngdoc function
- * @name hubAppApp.controller:MainCtrl
+ * @name hubAppApp.controller:SignUpCtrl
  * @description
- * # MainCtrl
+ * # SignUpCtrl
  * Controller of the hubAppApp
  */
 angular.module('hubAppApp').controller('SignUpCtrl', function($scope) {
 
-    $scope.basicForm = {
-    	"username": '',
-    	"firstName": '',
-    	"lastName": '',
-    	"email": '',
-    	"password": '',
-    	"retypedPassword": ''
+    $scope.retypedPassword = '';
+
+    $scope.signUpForm = {
+        "last_name": '',
+        "email": '',
+        "first_name": '',
+        "username": '',
+        "password": '',
+        "type": 'standard',
+        "phone": '',
+        "title": ''
     };
 
-    $scope.studentForm = {
-    	"city": '',
-    	"state": '',
-    	"zipCode": '',
-    	"facebook": '',
-    	"twitter": '',
-    	"googleId": '',
-    	"gravatar": '',
-    	"institution": '',
-    	"department": ''
+    $scope.settingsForm = {
+        "zip": '',
+        "twitter_url": '',
+        "google_url": '',
+        "address_1": '',
+        "address_2": '',
+        "state": '',
+        "country": '',
+        "city": '',
+        "facebook_url": ''
     };
 
-    $scope.userForm = {
-    	"title": '',
-    	"city": '',
-    	"state": '',
-    	"zipCode": '',
-    	"facebook": '',
-    	"twitter": '',
-    	"googleId": '',
-    	"gravatar": ''
+    $scope.tabs = {
+        "showBasicForm": true,
+        "showStudentForm": false,
+        "showUserForm": false
     };
-    
-    $scope.submit = function() {
-    	console.log($scope.basicForm);
-    	console.log($scope.studentForm);
-    	console.log($scope.userForm);
+
+    $scope.showBasicForm = function() {
+        $scope.tabs.showBasicForm = true;
+        $scope.tabs.showStudentForm = false;
+        $scope.tabs.showUserForm = false;
     }
+
+    $scope.showStudentForm = function() {
+        $scope.tabs.showBasicForm = false;
+        $scope.tabs.showStudentForm = true;
+        $scope.tabs.showUserForm = false;
+    }
+
+    $scope.showUserForm = function() {
+        $scope.tabs.showBasicForm = false;
+        $scope.tabs.showStudentForm = false;
+        $scope.tabs.showUserForm = true;
+    }
+
+    $scope.submit = function() {
+        console.log($scope.signUpForm);
+        console.log($scope.settingsForm);
+    }
+
     $scope.$on('$viewContentLoaded', function() {
         document.getElementById("primaryNav").style.backgroundColor = "rgba(0, 0, 0, 0.74)";
     });
