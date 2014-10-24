43fad36a143773d3d8190dea84cdd7b08577ef95
luisvalcourt2 <luis.valcourt@upr.edu>
2014-10-24 17:36:09 -0400

    BooK Profile - Changes 80%

--- app/scripts/controllers/bookProfile.js
+++ app/scripts/controllers/bookProfile.js
@@ -8,12 +8,42 @@
  * Controller of the hubAppApp
  */
 angular.module('hubAppApp').controller('bookProfileCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+   
+
+ $scope.bookInformation = {
+
+ 		"title": 'Harry Potter - Eat You Alive',
+        "author": 'Howard T. James',
+        "isbn": '012345667890',
+        "rating": '5.0',
+     	"publisher": 'Warner Brothers',
+     	"edition": '10th edition',
+        "price": '$249.99',
+        "reviews": '',
+
+        "recomendation": ''
+      
+    };
+
+     $scope.vendorInformation = {
+        "vendor": 'Luis N. Valcourt',
+        "rating": '4.9',
+        "address": 'Puerto Rico',
+        "description": 'Book in excellent condition.'
+      
+    };
+
+
+    $scope.offerInformation = {
+    	"quantity": '2',
+    	"new": '15',
+    	"used": '3'
+    };
+
+  }
+
+
+  );
 
 
 
