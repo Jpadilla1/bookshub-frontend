b6b960908cc529a1f70c626c39d46b6b7e6f8de7
luisvalcourt2 <luis.valcourt@upr.edu>
2014-10-30 17:15:28 -0400

    UserProfile  Changes

--- app/scripts/controllers/userProfile.js
+++ app/scripts/controllers/userProfile.js
@@ -18,4 +18,69 @@ angular.module('hubAppApp').controller('UserCtrl', function($scope) {
         document.getElementById("primary-nav").style.backgroundColor = "rgba(0, 0, 0, 0.74)";
         document.getElementById("brand-logo").src = "../images/logo.png";
     });
+
+
+ $scope.bookInformation = {
+
+ 		"title": 'Harry Potter - Eat You Alive',
+        "author": 'Howard T. James',
+        "isbn": '012345667890',
+        "rating": '5.0',
+     	"publisher": 'Warner Brothers',
+     	"edition": '10th edition',
+        "price": '$249.99',
+        "reviews": '',
+
+        "recomendation": ''
+      
+    };
+
+     $scope.vendorInformation = {
+        "vendor": 'Luis N. Valcourt',
+        "rating": '4.9',
+        "address": 'Puerto Rico',
+        "description": 'Book in excellent condition.'
+      
+    };
+
+
+    $scope.offerInformation = {
+    	"quantity": '2',
+    	"new": '15',
+    	"used": '3'
+    };
+
+  
+
+      $scope.tabs = {
+        "showNew": false,
+        "showUsed": false,
+        "showReview": true
+    };
+
+    $scope.showNew = function() {
+        $scope.tabs.showNew = true;[]
+        $scope.tabs.showUsed = false;
+        $scope.tabs.showReview = false;
+    }
+
+    $scope.showUsed = function() {
+        $scope.tabs.showNew = false;
+        $scope.tabs.showUsed = true;
+        $scope.tabs.showReview = false;
+    }
+
+    $scope.showReview = function() {
+        $scope.tabs.showNew = false;
+        $scope.tabs.showUsed = false;
+        $scope.tabs.showReview = true;
+    }
+
+
+
+
+
+
+
+    
 });
