8bd567c718590bac5854ec8f7f3b41bd598d22e9
luisvalcourt2 <luis.valcourt@upr.edu>
2014-10-30 20:04:18 -0400

    UserProfile

--- app/scripts/controllers/userProfile.js
+++ app/scripts/controllers/userProfile.js
@@ -20,63 +20,104 @@ angular.module('hubAppApp').controller('UserCtrl', function($scope) {
     });
 
 
- $scope.bookInformation = {
+    $scope.userInformation = {
 
- 		"title": 'Harry Potter - Eat You Alive',
-        "author": 'Howard T. James',
-        "isbn": '012345667890',
+ 		
+        "user": 'Howard T. James',
         "rating": '5.0',
-     	"publisher": 'Warner Brothers',
-     	"edition": '10th edition',
-        "price": '$249.99',
-        "reviews": '',
-
-        "recomendation": ''
-      
+        "review": 'Nice Seller, recommended with eyes closed!',
+        "type": 'Seller',
+        "facebook_url": 'www.facebook.com',
+        "twitter_url": '',
+        "github_url": ''
+        
     };
 
-     $scope.vendorInformation = {
-        "vendor": 'Luis N. Valcourt',
-        "rating": '4.9',
-        "address": 'Puerto Rico',
-        "description": 'Book in excellent condition.'
-      
+    $scope.userBook = {
+
+    	"quantity": '15',
+    	"new": '10',
+    	"used": '5'	
+
+
     };
 
 
-    $scope.offerInformation = {
-    	"quantity": '2',
-    	"new": '15',
-    	"used": '3'
+    $scope.userReview = {
+
+    	"user": 'Emmanuel Cleaveland',
+    	"quantity": '1'
+
     };
+
+
 
   
 
       $scope.tabs = {
         "showNew": false,
         "showUsed": false,
-        "showReview": true
+        "showReview": true,
+        "showContactForm": false,
+        "showRating": true,
+        "showInformation": true,
+  
     };
+
+     $scope.showInformation = function() {
+        $scope.tabs.showNew = false;
+        $scope.tabs.showUsed = false;
+        $scope.tabs.showReview = true;
+        $scope.tabs.showRating = true;
+        $scope.tabs.showInformation = true
+        $scope.tabs.showContactForm = false;
+  
+    }
+
+
+    $scope.showRating = function() {
+        $scope.tabs.showNew = false;
+        $scope.tabs.showUsed = false;
+        $scope.tabs.showReview = true;
+        $scope.tabs.showRating = false;
+        $scope.tabs.showContactForm = false;
+        $scope.tabs.showInformation = true;
+  
+    }
 
     $scope.showNew = function() {
         $scope.tabs.showNew = true;[]
         $scope.tabs.showUsed = false;
         $scope.tabs.showReview = false;
+        $scope.tabs.showContactForm = false;
+         $scope.tabs.showInformation = true;
     }
 
     $scope.showUsed = function() {
         $scope.tabs.showNew = false;
         $scope.tabs.showUsed = true;
         $scope.tabs.showReview = false;
+        $scope.tabs.showContactForm = false;
+         $scope.tabs.showInformation = true;
     }
 
     $scope.showReview = function() {
         $scope.tabs.showNew = false;
         $scope.tabs.showUsed = false;
         $scope.tabs.showReview = true;
+        $scope.tabs.showContactForm = false;
+        $scope.tabs.showInformation = true;
     }
 
-
+    $scope.showContactForm = function() {
+        $scope.tabs.showNew = false;
+        $scope.tabs.showUsed = false;
+        $scope.tabs.showReview = true;
+        $scope.tabs.showRating = false;
+        $scope.tabs.showContactForm = true;
+        $scope.tabs.showInformation = false;
+  
+    }
 
 
 
