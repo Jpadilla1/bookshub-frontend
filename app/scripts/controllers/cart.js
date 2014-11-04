1852c59700392d4f09a2b300afe13e41ca22f3a8
Carl1101 <carlosvazquez014@gmail.com>
2014-11-04 02:06:11 -0400

    Created Cart

--- app/scripts/controllers/cart.js
+++ app/scripts/controllers/cart.js
@@ -9,6 +9,16 @@
  */
 angular.module('hubAppApp').controller('CartCtrl', function($scope) {
 
+	$scope.cartItems = [
+	{"title": 'Starting Out With Java',
+		"isbn13": '978-0132855839',
+		"isbn10": 0132855836,
+		"author": 'Tonny Gaddis',
+		"quantity": '2',
+		"pricePerItem": 40
+	}
+	];
+
     $scope.$on('$viewContentLoaded', function() {
 		defaultNavbar();
     });
