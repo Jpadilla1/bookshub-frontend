35295533a438c20592b52338d55d0c6f6861a2ef
Carl1101 <carlosvazquez014@gmail.com>
2014-11-04 18:51:18 -0400

    Added some design and approach changes

--- app/scripts/controllers/book.js
+++ app/scripts/controllers/book.js
@@ -10,7 +10,11 @@
 angular.module('hubAppApp').controller('BookCtrl', function($scope) {
 
     $scope.searchInput = '';
-
+    $scope.show = {
+        "searchForm": true,
+        "newForm": false,
+        "result": false
+    };
     $scope.newBookForm = {
         "title": '',
         "isbn": '',
@@ -18,11 +22,28 @@ angular.module('hubAppApp').controller('BookCtrl', function($scope) {
         "edition": '',
         "category": ''
     };
+    $scope.searchResult = [{
+        "title": 'Starting Out With Java',
+        "isbn13": '978-0132855839',
+        "isbn10": 0132855836,
+        "author": 'Tonny Gaddis',
+        "edition": '4th',
+        "publisher": 'Pearson'
+    }, {
+        "title": 'Starting Out With Java',
+        "isbn13": '978-0132855839',
+        "isbn10": 0132855836,
+        "author": 'Tonny Gaddis',
+        "edition": '4th',
+        "publisher": 'Pearson'
+    }];
+
+    $scope.searchIsMoved = false;
 
     var originalNewBookForm = angular.copy($scope.newBookForm);
 
     $scope.submit = function() {
-        
+
     }
 
     $scope.clear = function() {
@@ -30,8 +51,15 @@ angular.module('hubAppApp').controller('BookCtrl', function($scope) {
         $scope.bookForm.$setPristine();
     }
 
+    $scope.notFound = function() {
+        $scope.show.result = !$scope.show.result;
+        $scope.show.searchForm = !$scope.show.searchForm;
+        $scope.show.newForm = !$scope.show.newForm;
+    }
+
     $scope.submitByTitle = function() {
-        alert("tile");
+        $scope.moveSearchForm();
+        $scope.show.result = !$scope.show.result;
     }
 
     $scope.submitByIsbn10 = function() {
@@ -44,6 +72,18 @@ angular.module('hubAppApp').controller('BookCtrl', function($scope) {
 
     $scope.submitByAuthor = function() {
         alert("author");
+    }
+
+    $scope.moveSearchForm = function() {
+        if ($scope.searchIsMoved) {
+            document.getElementById("add-book-search-form").style.marginTop = "35%";
+            document.getElementById("add-book-search-form").style.marginBottom = "30%";
+            $scope.searchIsMoved = false;
+        } else {
+            document.getElementById("add-book-search-form").style.marginTop = "0%";
+            document.getElementById("add-book-search-form").style.marginBottom = "5%";
+            $scope.searchIsMoved = true;
+        }
     }
 
     $scope.$on('$viewContentLoaded', function() {
