0fadfa57c0489c5fed18b51257f830f30210f34b
Carl1101 <carlosvazquez014@gmail.com>
2014-11-04 09:44:22 -0400

    Added some Angular.js

--- app/scripts/controllers/cart.js
+++ app/scripts/controllers/cart.js
@@ -16,10 +16,35 @@ angular.module('hubAppApp').controller('CartCtrl', function($scope) {
 		"author": 'Tonny Gaddis',
 		"quantity": '2',
 		"pricePerItem": 40
+	},
+	{"title": 'Starting Out With Java',
+		"isbn13": '978-0132855839',
+		"isbn10": 0132855836,
+		"author": 'Tonny Gaddis',
+		"quantity": '2',
+		"pricePerItem": 40
 	}
 	];
 
+	$scope.totalPrice = 0;
+
+	$scope.checkTotalPrice = function(){
+		var sum = 0;
+		angular.forEach($scope.cartItems,function(item){
+			sum += (item.quantity * item.pricePerItem); 		
+		});
+		$scope.totalPrice = sum;
+	}
+
+	$scope.removeAll = function() {
+		$scope.cartItems = [];
+		$scope.checkTotalPrice();
+		document.getElementById("select-all").checked = false;
+		$scope.allItemsClicked = false;
+	}
+
     $scope.$on('$viewContentLoaded', function() {
 		defaultNavbar();
+		$scope.checkTotalPrice();
     });
 });
