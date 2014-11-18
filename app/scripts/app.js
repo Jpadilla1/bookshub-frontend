55738f80c67f540160fd3b872f63ce30cbec3a0b
luisvalcourt2 <luis.valcourt@upr.edu>
2014-11-18 17:16:19 -0400

    Merge branch 'develop' of https://github.com/Jpadilla1/bookshub-frontend into develop
    Conflicts:
    	app/scripts/controllers/requested.js
    	app/views/requested.html

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -35,6 +35,10 @@ app.config(function($routeProvider) {
        templateUrl: 'views/about.html',
        controller: 'AboutCtrl'
      })
+     .when('/contactus', {
+       templateUrl: 'views/contactus.html',
+       controller: 'ContactUsCtrl'
+     })
      .when('/user-profile', {
        templateUrl: 'views/userProfile.html',
        controller: 'UserCtrl'
@@ -42,6 +46,10 @@ app.config(function($routeProvider) {
       .when('/book', {
        templateUrl: 'views/book.html',
        controller: 'BookCtrl'
+     })
+      .when('/requested', {
+       templateUrl: 'views/requested.html',
+       controller: 'RequestCtrl'
      })
       .when('/login', {
        templateUrl: 'views/login.html',
@@ -62,6 +70,10 @@ app.config(function($routeProvider) {
       .when('/cart', {
       templateUrl: 'views/cart.html',
       controller: 'CartCtrl'
+     })
+      .when('/settings', {
+      templateUrl: 'views/settings.html',
+      controller: 'SettingsCtrl'
      })
       .when('/specific-profile', {
       templateUrl: 'views/specificUserProfile.html',
--- app/scripts/controllers/requested.js
+++ app/scripts/controllers/requested.js
@@ -10,8 +10,16 @@
 
 var app = angular.module('hubAppApp');
 
+<<<<<<< HEAD
 app.controller('RequestCtrl', ['$scope', 'MyBookService', function($scope, MyBookService) {
 
+=======
+app.controller('RequestCtrl', 
+    ['$rootScope', '$scope', 'authService', 'MyContactService', 'UserService',
+     'MyOfferService', 'MyBookService', 
+     function($rootScope, $scope, authService, MyContactService,
+        UserService, MyOfferService, MyBookService) {
+>>>>>>> FETCH_HEAD
 
     $scope.AllRequested = true;
     $scope.TopRequested = false;
@@ -19,29 +27,149 @@ app.controller('RequestCtrl', ['$scope', 'MyBookService', function($scope, MyBoo
     $scope.showAllRequested = function() {
 
         $scope.result = MyBookService.booksRequested.get();
+<<<<<<< HEAD
            console.log($scope.result);
 
         
 
+=======
+        
+>>>>>>> FETCH_HEAD
         $scope.AllRequested = true;
         $scope.TopRequested = false;
     };
 
     $scope.showTopRequested = function() {
 
+<<<<<<< HEAD
          $scope.topRequested = MyBookService.topRequested.get();
 
         $scope.AllRequested = false;
         $scope.TopRequested = true;
 
 
+=======
+        $scope.topRequested = MyBookService.topRequested.get();
+
+        $scope.AllRequested = false;
+        $scope.TopRequested = true;
+>>>>>>> FETCH_HEAD
     
     };
     
     $scope.showAllRequested();
+<<<<<<< HEAD
 
 
       $scope.$on('$viewContentLoaded', function() {
+=======
+    
+    $scope.tabs = {
+        "showNew": false,
+        "showUsed": false,
+        "showReview": true,
+        "showInformation": true,
+        "showOffers": false,
+        "showAllOffers": false,
+        "offersNavbar": false
+    };
+
+    $scope.showNew = function() {
+        $scope.tabs.showNew = true;
+        $scope.tabs.showUsed = false;
+        $scope.tabs.showReview = false;
+        $scope.tabs.showContactForm = false;
+        $scope.tabs.showInformation = true;
+        $scope.tabs.showRating = true;
+        $scope.tabs.showReviewForm = false;
+        $scope.tabs.offersNavbar = true;
+    }
+
+    $scope.showUsed = function() {
+        $scope.tabs.showNew = false;
+        $scope.tabs.showUsed = true;
+        $scope.tabs.showRating = true;
+        $scope.tabs.showReview = false;
+        $scope.tabs.showContactForm = false;
+        $scope.tabs.showInformation = true;
+        $scope.tabs.showReviewForm = false;
+        $scope.tabs.offersNavbar = true;
+    }
+
+    $scope.showReview = function() {
+        $scope.tabs.showNew = false;
+        $scope.tabs.showUsed = false;
+        $scope.tabs.showReview = true;
+        $scope.tabs.showRating = true;
+        $scope.tabs.offersNavbar = false;
+    }
+
+    $scope.showOffers = function() {
+        $scope.tabs.showNew = true;
+        $scope.tabs.showUsed = false;
+        $scope.tabs.showReview = false;
+        $scope.tabs.showRating = false;
+        $scope.tabs.showAllOffers = true;
+        $scope.tabs.offersNavbar = true;
+    }
+
+    $scope.showReviews = function() {
+        $scope.tabs.showNew = false;
+        $scope.tabs.showUsed = false;
+        $scope.tabs.showReview = true;
+        $scope.tabs.showRating = false;
+        $scope.tabs.showContactForm = false;
+        $scope.tabs.showInformation = false;
+        $scope.tabs.offersNavbar = false;
+    }
+
+    $scope.userProfile = '';
+
+    $scope.currentUserInformation = '';
+
+    $scope.userReviews = '';
+
+    $scope.offers = '';
+
+    $scope.bookInformation = '';
+
+    authService.settings().then(function(data) {
+        $scope.currentUserInformation = data;
+        $scope.addReviewData();
+        $scope.addOfferData();
+    });
+
+    $scope.addReviewData = function() {
+        var params = {
+            'userId': $scope.currentUserInformation.id
+        };
+
+        $scope.userReviews = UserService.userReview.get(params);
+        console.log($scope.userReviews);
+    };
+
+    $scope.addOfferData = function(){
+        var offerParams = {
+            'ownerId': $scope.currentUserInformation.id
+        };
+
+        $scope.offers = MyOfferService.userOffers.get(offerParams);
+
+        console.log($scope.offers);
+    };
+
+    $scope.submitReview = function() {
+        $scope.newReview.created_by = $scope.newReview.user_id = $scope.userProfile.id;
+        $scope.owner = '3';
+        UserService.userReview.save('', $scope.newReview);
+    };
+
+    $scope.goToOtherUser = function(userId){
+        UserService.setUserId(userId);
+    };
+
+    $scope.$on('$viewContentLoaded', function() {
+>>>>>>> FETCH_HEAD
         defaultNavbar();
     });
 }]);
