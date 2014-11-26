561925309b7a721ce9e2c9d1be3a79b34f16a9b1
Emmanuel Luciano <emma.luciano11@gmail.com>
2014-11-26 09:10:19 -0400

    navbar manipulation now works properly

--- app/scripts/js/navbarManipulation.js
+++ app/scripts/js/navbarManipulation.js
@@ -3,12 +3,23 @@ function homeNavbar(){
     navbar.style.backgroundColor = "transparent";
     navbar.style.borderBottomColor = "transparent";
 
-    var anchors = document.getElementById("primary-nav").getElementsByTagName('a');
-    for (var i = 0; i < anchors.length; i++) {
-        anchors[i].style.color = "white";
+    // var anchors = document.getElementById("icon-links").getElementsByTagName('a');
+    // for (var i = 0; i < anchors.length; i++) {
+    //     anchors[i].style.color = "white";
+    // };
+
+    var achors = document.getElementById('navbar-options').getElementsByTagName('a');
+
+    for (var i = 0; i < achors.length; i++) {
+        if(achors[i].id == 'search-options'){
+            achors[i].style.color = "black";
+        }else{
+            achors[i].style.color = "white";
+        }
     };
 
-    var icons = document.getElementById("primary-nav").getElementsByTagName('i');
+    var icons = document.getElementById('navbar-options').getElementsByTagName('i');
+
     for (var i = 0; i < icons.length; i++) {
         icons[i].style.color = "white";
     };
