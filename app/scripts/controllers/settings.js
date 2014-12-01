7f2e099b173ebdef61ca941b416bec2ed8d7d9a5
Emmanuel Luciano <emma.luciano11@gmail.com>
2014-12-01 18:14:40 -0400

    muchas cosas

--- app/scripts/controllers/settings.js
+++ app/scripts/controllers/settings.js
@@ -2,44 +2,81 @@
 
 /**
  * @ngdoc function
- * @name hubAppApp.controller:AboutCtrl
+ * @name hubAppApp.controller:SignUpCtrl
  * @description
- * # AboutCtrl
+ * # SignUpCtrl
  * Controller of the hubAppApp
  */
 
-
 var app = angular.module('hubAppApp');
 
-app.controller('SettingsCtrl', function($scope) {
-      
-      
-      
+app.controller('SettingsCtrl', ['$scope', 'MyOfferService', 'authService', function($scope, MyOfferService, authService) {
+  $scope.tabs = {
+    "showUserInformation": true,
+    "showOffers": false,
+    "editOffer": false
+  };
 
-     $scope.showUserInformation1 = function() {
-      console.log("hi");
-        $scope.showUserInformation = true;
-        $scope.showBookInformation = false;
-        
-    };
+  $scope.userOffers = '';
+  $scope.userData = ''; 
+  $scope.offerEdit = {
+    'description': '',
+    'condition': '',
+    'price': '',
+    'quantity': ''
+  };
 
-     $scope.showBookInformation1 = function() {
-        $scope.showBookInformation = true;
-         $scope.showUserInformation = false;
-    };
+  authService.settings().then(function(data){
+    $scope.userData = data;
+  });
 
-    var init = function() {
-      $scope.showUserInformation = true;
-      $scope.showBookInformation = false;
+  $scope.showPersonalInformation = function() {
+    $scope.tabs.showUserInformation = true;
+    $scope.tabs.showOffers = false;
+
+    if(!($('#settings-personal').hasClass('active'))){
+      $('#settings-personal').addClass('active');
+      $('#settings-offers').removeClass('active');
+    }
+  };
+
+  $scope.showOffersInformation = function() {
+    $scope.tabs.showUserInformation = false;
+    $scope.tabs.showOffers = true;
+
+    if(!($('#settings-offers').hasClass('active'))){
+      $('#settings-offers').addClass('active');
+      $('#settings-personal').removeClass('active');
     }
 
+    var params = {
+      "ownerId": $scope.userData.id
+    };
+
+    MyOfferService.userOffers.get(params).$promise.then(function(data){
+      $scope.userOffers = data;
+      console.log($scope.userOffers);
+    });
+  };
+
+  $scope.showEditOfferForm = function(id){
+    var params = {
+      'offerId': id,
+    };
+
+    MyOfferService.bookOffer.get(params).$promise.then(function(data){
+      console.log(data);
+      $scope.offerEdit.description = data.description;
+      $scope.offerEdit.condition = data.condition;
+      $scope.offerEdit.price = data.price;
+      $scope.offerEdit.quantity = data.quantity;
+      console.log($scope.offerEdit);
+    });
+
+    $scope.tabs.editOffer = true;
+  };
 
  $scope.$on('$viewContentLoaded', function() {
-        defaultNavbar();
-    });
-
-
-      
-      init();
-     
-    });
+    defaultNavbar();
+  });
+}]);
