721dff5f303236c0a9705d11e0ced34176fa2f77
Carl1101 <carlosvazquez014@gmail.com>
2014-12-01 20:23:03 -0400

    Created All users page and added logic

--- app/scripts/controllers/showAllUsers.js
+++ app/scripts/controllers/showAllUsers.js
@@ -9,8 +9,30 @@
  */
 angular.module('hubAppApp')
     .controller('ShowAllUsersCtrl', function($scope) {
-        
-        
+
+        $scope.searchInput;
+        $scope.results = [{
+            'name': "Juan",
+            'description': "Baila tu cuerpo alegria macarena, alegria cosa buena, ehhhhh macarena!!"
+        }, {
+            'name': "Pedro",
+            'description': "Baila tu cuerpo alegria macarena, alegria cosa buena, ehhhhh macarena!!"
+
+        }, {
+            'name': "Maria",
+            'description': "Baila tu cuerpo alegria macarena, alegria cosa buena, ehhhhh macarena!!"
+        }, {
+            'name': "Jose",
+            'description': "Baila tu cuerpo alegria macarena, alegria cosa buena, ehhhhh macarena!!"
+        }, {
+            'name': "Emmanuel",
+            'description': "Baila tu cuerpo alegria macarena, alegria cosa buena, ehhhhh macarena!!"
+        }];
+
+        $scope.getAllUsers = function() {
+            
+        }
+
         $scope.$on('$viewContentLoaded', function() {
             defaultNavbar();
         });
